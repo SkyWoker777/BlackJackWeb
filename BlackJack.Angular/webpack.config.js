4cfbff140540233445f1428d6067430c2f063b34
Dmitry <skywoker33@hotmail.com>
2018-09-30 12:51:19 +0300

    Add project files.

---
+++ BlackJack.Angular/webpack.config.js
@@ -0,0 +1,64 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // плагин минимизации
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    entry: {
+        'polyfills': './ClientApp/src/polyfills.ts',
+        'app': './ClientApp/src/main.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, './public'),     // путь к каталогу выходных файлов - папка public
+        publicPath: '/public/',
+        filename: "[name].js"       // название создаваемого файла
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [   //загрузчик для ts
+            {
+                test: /\.ts$/, // определяем тип файлов
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: { configFileName: path.resolve(__dirname, 'tsconfig.json') }
+                    },
+                    'angular2-template-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }, {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+            }, {
+                test: /\.css$/,
+                include: path.resolve(__dirname, 'ClientApp/src/app'),
+                loader: 'raw-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core/,
+            path.resolve(__dirname, 'ClientApp/src'), // каталог с исходными файлами
+            {} // карта маршрутов
+        ),
+        new HtmlWebpackPlugin({
+            template: 'ClientApp/src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new UglifyJSPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            htmlLoader: {
+                minimize: false
+            }
+        })
+    ]
+}
